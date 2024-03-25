ab30038c261c9919e95d760b783f703fe1c87c43
SzymonStelware <salmedd55@gmail.com>
2024-03-25 13:38:55 +0100

    sync

--- api/internal-call-api-v1.py
+++ api/internal-call-api-v1.py
@@ -1,23 +1,16 @@
 import requests
-requests.path.append('/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/requests')
+from flask import Flask, jsonify
 
-#from flask import Flask, jsonify
+app = Flask(__name__)
 
-#app = Flask(__name__)
+@app.route('/api/response', methods=['GET'])
 
-#@app.route('/api/response', methods=['GET'])
-
-#def get_data():
-   # url = "https://malydevops.pl"
+def get_data():
+    url = "https://malydevops.pl"
     
-   # response = requests.get(url)
-   # response_json = response.json()
-   # print(response_json)
-    #return jsonify(response_json)
-
-#if __name__ == '__main__':
- #   app.run(debug=True)
-
-url = "https://malydevops.pl"
-response = requests.get(url)
-print(response.status_code)
+    response = requests.get(url)
+    response_json = response.json()
+    return jsonify(print(response_json))
+    #return jsonify(response)
+if __name__ == '__main__':
+    app.run(debug=True, port=1234)
